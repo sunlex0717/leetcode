bcc02510c847b93cae52ac730bdec37f29412739
SUNLEX\Administrator <sunwei19950327@gmail.com>
2018-06-30 17:41:29 +0200

    temp save

---
+++ Explore/ArrayAndString/DiagonalTraverse.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+        //get size of matrix, M*N   
+        int num_row = matrix.size();
+        int num_col = matrix[0].size(); //because they are same  
+    }
+};
