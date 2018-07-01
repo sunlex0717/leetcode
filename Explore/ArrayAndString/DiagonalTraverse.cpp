de0085f948e4163421ae7c1258e5a99c70e6e5db
SUNLEX\Administrator <sunwei19950327@gmail.com>
2018-07-01 22:56:06 +0200

    add

--- Explore/ArrayAndString/DiagonalTraverse.cpp
+++ Explore/ArrayAndString/DiagonalTraverse.cpp
@@ -1,8 +1,33 @@
+/*
+I did not come up with a efficient solution, it is from other coder, a very smart solution;
+*/
+
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
         //get size of matrix, M*N   
+        if(matrix.empty()){
+            return {} ;
+        }
         int num_row = matrix.size();
-        int num_col = matrix[0].size(); //because they are same  
+        int num_col = matrix[0].size(); //because they are same 
+       
+        vector<int> ret;
+        for(int i = 0; i <= num_col+num_row - 2;i++){
+
+            for(int j = 0; j <= i ;j++ ){
+            int x = j; //row
+            int y = i-x; //col
+            //
+            if((i%2 == 0)){ //up
+                swap(x,y);    
+            }
+            if( (x >= num_row) || (y >= num_col)){
+                continue;
+            }
+            ret.push_back(matrix[x][y]);
+            }
+        }
+        return ret;
     }
 };
---
+++ Explore/ArrayAndString/SpiralMatrix.cpp
@@ -0,0 +1,39 @@
+/*
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+Input:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+*/
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        //get size of matrix, M*N   
+        if(matrix.empty()){
+            return {} ;
+        }
+        int num_row = matrix.size();
+        int num_col = matrix[0].size(); //because they are same 
+
+        int min_row_col = min(num_col,num_row);
+
+        
+
+    }
+};
