75323c5a9e6bf75bd8df2abf38bcfa5a5354ff1f
sunlex0717 <sun-wei1995@outlook.com>
2018-07-03 10:41:50 +0200

    finish pascaltriangle

--- Explore/ArrayAndString/PascalTriangle.cpp
+++ Explore/ArrayAndString/PascalTriangle.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        //corner case
         if(numRows == 0){
             return {};
         }
         if(numRows == 1){
-            vector<int> a(1,1);
+            vector<vector<int>> a(1);
+            a[0]=vector<int>(1,1);
+            return a;
+        }
+        if(numRows == 2){
+            vector<vector<int>> a(2);
+            a[0] = vector<int>(1,1);
+            a[1] = vector<int>(2,1);
             return a;
         }
         // initialize
@@ -18,5 +26,11 @@ public:
         
 
         //TODO: body code, to fill the triangle
+        for(int i = 2;i<numRows;++i){ //row
+            for (int j = 1 ;j <=i-1;++j){
+                res[i][j] = res[i-1][j-1]+res[i-1][j];
+            }
+        }
+        return res;
     }
 };
