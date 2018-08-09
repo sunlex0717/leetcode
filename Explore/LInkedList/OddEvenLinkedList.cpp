e435c371dc7fa723df4682f9659aa1c4a33870b1
sunlex <sunwei19950327@gmail.com>
2018-08-09 08:59:26 +0200

    add practices

--- Explore/LInkedList/OddEvenLinkedList.cpp
+++ Explore/LInkedList/OddEvenLinkedList.cpp
@@ -7,10 +7,21 @@
  * };
  */
 // 
+// two pointer tech, reorder odd and even respectively and then link them together 
 class Solution {
 public:
-    ListNode* oddEvenList(ListNode* head) {
-        ListNode* odd_p = head;
-        ListNode* even_p = head->next;
+   ListNode* oddEvenList(ListNode* head) 
+    {
+        if(!head) return head;
+        ListNode *odd=head, *evenhead=head->next, *even = evenhead;
+        while(even && even->next)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenhead;
+        return head;
     }
 };
